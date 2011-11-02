9e1b8ce679cda5b91f79f7aa36a6c6d986a0b6ee
Gert Goet <gert@thinkcreate.nl>
2011-11-02 23:31:06 +0100

    wrap test-app in rack::lint

--- spec/rack/pjax_spec.rb
+++ spec/rack/pjax_spec.rb
@@ -4,24 +4,29 @@ describe Rack::Pjax do
   include Rack::Test::Methods # can be moved to config
 
   def app
-    Rack::Builder.app do
-      use Rack::Pjax
-      run lambda { |env|
-        body = '<html><title>Hello</title><body><div data-pjax-container>World!</div></body></html>'
-        headers = {"Content-Length" => Rack::Utils.bytesize(body).to_s}
-        [200, headers, [body]]
-      }
-    end
+    Rack::Lint.new(
+      Rack::Builder.app do
+        use Rack::Pjax
+        run lambda { |env|
+          body = '<html><title>Hello</title><body><div data-pjax-container>World!</div></body></html>'
+          headers = {
+            "Content-Length" => Rack::Utils.bytesize(body).to_s,
+            "Content-Type" => 'text/plain'
+          }
+          [200, headers, [body]]
+        }
+      end
+    )
   end
 
   context "when receiving a pjax-request" do
     it "should return title-tag and inner-html of the pjax-container" do
-      get "/", {}, {'HTTP_X_PJAX' => true}
+      get "/", {}, {"HTTP_X_PJAX" => "true"}
       body.should == "<title>Hello</title>World!"
     end
 
-    it "should recalculate the Content Length" do
-      get "/", {}, {'HTTP_X_PJAX' => true}
+    it "should have the correct Content Length" do
+      get "/", {}, {"HTTP_X_PJAX" => "true"}
       headers['Content-Length'].should == Rack::Utils.bytesize(body).to_s
     end
   end
