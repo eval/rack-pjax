9c69ac2422ff61d0d91b233259c94c2e8579a0bf
Gert Goet <gert@thinkcreate.nl>
2012-08-28 23:56:20 +0200

    Some adjustments

--- lib/rack/pjax.rb
+++ lib/rack/pjax.rb
@@ -10,32 +10,34 @@ module Rack
 
     def call(env)
       status, headers, body = @app.call(env)
+      return [status, headers, body] unless pjax?(env)
+
       headers = HeaderHash.new(headers)
 
-      if pjax?(env)
-        new_body = ""
-        body.each do |b|
-          b.force_encoding('UTF-8') if RUBY_VERSION > '1.9.0'
-          parsed_body = Nokogiri::HTML(b)
-          container = parsed_body.at("[@data-pjax-container]")
+      new_body = ""
+      body.each do |b|
+        b.force_encoding('UTF-8') if RUBY_VERSION > '1.9.0'
+
+        parsed_body = Nokogiri::HTML(b)
+        container = parsed_body.at("[@data-pjax-container]")
+
+        new_body << begin
           if container
             title = parsed_body.at("title")
 
-            new_body << title.to_s if title
-            new_body << container.inner_html
+            "%s%s" % [title, container.inner_html]
           else
-            new_body << b
+            b
           end
         end
-
-        body.close if body.respond_to?(:close)
-        body = [new_body]
-
-        headers['Content-Length'] &&= bytesize(new_body).to_s
-        request = Rack::Request.new(env)
-        headers['X-PJAX-URL'] = request.fullpath
       end
-      [status, headers, body]
+
+      body.close if body.respond_to?(:close)
+
+      headers['Content-Length'] &&= bytesize(new_body).to_s
+      headers['X-PJAX-URL'] = Rack::Request.new(env).fullpath
+
+      [status, headers, [new_body]]
     end
 
     protected
