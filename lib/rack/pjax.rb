9724779d0eb82b0c19a0f5dae2eaefe7be331bab
Gert Goet <gert@thinkcreate.nl>
2012-09-26 09:04:10 +0200

    Rebase

--- lib/rack/pjax.rb
+++ lib/rack/pjax.rb
@@ -17,9 +17,9 @@ module Rack
       new_body = ""
       body.each do |b|
         b.force_encoding('UTF-8') if RUBY_VERSION > '1.9.0'
-
+          
         parsed_body = Nokogiri::HTML(b)
-        container = parsed_body.at("[@data-pjax-container]")
+        container = parsed_body.at(container_selector(env))
 
         new_body << begin
           if container
@@ -43,6 +43,10 @@ module Rack
     protected
       def pjax?(env)
         env['HTTP_X_PJAX']
+      end
+
+      def container_selector(env)
+        env['HTTP_X_PJAX_CONTAINER'] || "[@data-pjax-container]"
       end
   end
 end
